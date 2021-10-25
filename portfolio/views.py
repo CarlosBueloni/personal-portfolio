0853ed69551df37d8e203151563d0086797fe874
CarlosBueloni <carlosbueloni@gmail.com>
2021-10-25 15:23:23 -0300

    Create link between Project model and templates

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def index(request):
-    return render(request, 'portfolio/index.html')
+    projects = Project.objects.all()
+    return render(request, 'portfolio/index.html', {'projects': projects})
