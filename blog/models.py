9766b3c6c6c82e6c1868312f5a0b336f72d4eb6b
CarlosBueloni <carlosbueloni@gmail.com>
2021-10-25 16:04:37 -0300

    Update blog model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=300)
-    date = models.DateField(auto_now_add=True)
-    url = models.URLField(blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
