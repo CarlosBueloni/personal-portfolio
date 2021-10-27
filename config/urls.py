b5b0cdaa9c9a0dee50cc73001393f593e1b645d2
CarlosBueloni <carlosbueloni@gmail.com>
2021-10-27 18:12:22 -0300

    Add app_name

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
 
+app_name = 'portfolio'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
