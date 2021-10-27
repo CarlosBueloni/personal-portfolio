91e4a66900d43cf969a935e1218d7893a5dd0a18
CarlosBueloni <carlosbueloni@gmail.com>
2021-10-26 22:59:59 -0300

    Add styling to templates

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Blog
 # Create your views here.
 def all_blogs(request):
     blog_count = Blog.objects.count
-    blogs = Blog.objects.order_by('-date')[:5]
+    blogs = Blog.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'blogs':blogs, 'blog_count':blog_count})
 
 def detail(request, blog_id):
